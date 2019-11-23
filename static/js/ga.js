d73a05bcc1d267b21abc37350bbed0903042e32f
Karthik T <karthik@accern.com>
2019-11-23 18:28:07 -0500

    use ganalytics instead of gtags

--- static/js/ga.js
+++ static/js/ga.js
@@ -1,4 +1,6 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'UA-98828142-2');
+console.log("we have google analytics yo!");
+window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+const trackingId = 'UA-98828142-2';
+ga('create', trackingId, 'auto');
+ga('send', 'pageview');
+console.log("it did not err!");
